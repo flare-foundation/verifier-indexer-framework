8ea1f71db13c03b79b456f75110c8a7a1be930f0
Ryan Collingham <ryan.collingham@flare.network>
2024-08-07 11:01:06 +0100

    fix primary key tags

--- pkg/database/entities.go
+++ pkg/database/entities.go
@@ -13,13 +13,13 @@ var entities = []interface{}{
 }
 
 type State struct {
-	ID                     int `gorm:"primaryKey;unique"`
+	ID                     int `gorm:"primaryKey"`
 	LastIndexedBlockNumber uint64
 	UpdatedAt              time.Time
 }
 
 type Block struct {
-	Hash      string `gorm:"type:varchar(66);index;unique"`
+	Hash      string `gorm:"primaryKey;type:varchar(66)"`
 	Number    uint64 `gorm:"index"`
 	Timestamp uint64 `gorm:"index"`
 
@@ -28,7 +28,7 @@ type Block struct {
 }
 
 type Transaction struct {
-	Hash        string `gorm:"primaryKey;type:varchar(66);index;unique"`
+	Hash        string `gorm:"primaryKey;type:varchar(66)"`
 	BlockHash   string `gorm:"type:varchar(66)"`
 	Block       *Block
 	FromAddress string `gorm:"type:varchar(42);index"`
